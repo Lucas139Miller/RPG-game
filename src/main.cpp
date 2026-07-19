e03c23be83ac1b315b0a4f4cb5c6960b0ce9ab1b
Lucas139Miller <lucas139miller@ufmg.br>
2026-07-18 21:19:51 -0300

    I got a moving box

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,16 @@ int main(int argc, char** args) {
 	// Initialize SDL. SDL_Init will return -1 if it fails.
 	if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
 		cout << "Error initializing SDL: " << SDL_GetError() << endl;
-		std::cin.get();
 		// End the program
 		return 1;
 	}
 
 	// Create our window
-	window = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+	window = SDL_CreateWindow( "RPG game -.-", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
 
 	// Make sure creating the window succeeded
 	if ( !window ) {
 		cout << "Error creating window: " << SDL_GetError()  << endl;
-		std::cin.get();
 
 		// End the program
 		return 1;
@@ -41,44 +39,65 @@ int main(int argc, char** args) {
 	// Make sure getting the surface succeeded
 	if ( !winSurface ) {
 		cout << "Error getting surface: " << SDL_GetError() << endl;
-		std::cin.get();
 		// End the program
 		return 1;
 	}
 
 
+	SDL_Event ev;
+    bool running = true;
+
+    int x,y,w,h;
+    x= 10;
+    y= 10;
+    w = 100;
+    h = 255;
+
+	//RUNNIG LOOP **************************************************************
+    while(running){
+        // Event loop **************************************************************
+
+        while ( SDL_PollEvent( &ev ) != 0 ) {
+            // Events
+            switch(ev.type){
+                case SDL_QUIT:
+                    return false;
+                case SDL_KEYDOWN:
+                    switch(ev.key.keysym.sym){
+                        case SDLK_a:
+                            x-=10;
+                            break;
+                        case SDLK_d:
+                            x+=10;
+                            break;
+                    }
+                break;
+            }
+        }
+
+        // Fill the window with a white rectangle
+        SDL_FillRect( winSurface, NULL, SDL_MapRGB( winSurface->format, 255, 255, 255 ) );
+
+        SDL_Rect rec;
+        rec.x = x;
+        rec.y = y;
+        rec.w = w;
+        rec.h = h;
+        SDL_FillRect( winSurface, &rec, SDL_MapRGB( winSurface->format, 0,255,0));
+        // Update the window display
+        SDL_UpdateWindowSurface( window );
 
 
+        // Wait before next frame
+        SDL_Delay(10);
+    }
 
-
-	// Fill the window with a white rectangle
-	SDL_FillRect( winSurface, NULL, SDL_MapRGB( winSurface->format, 255, 255, 255 ) );
-
-    SDL_Rect rec;
-    rec.x = 10;
-    rec.y = 10;
-    rec.w = 100;
-    rec.h = 225;
-    SDL_FillRect( winSurface, &rec, SDL_MapRGB( winSurface->format, 0,255,0));
-	// Update the window display
-	SDL_UpdateWindowSurface( window );
-
-	// Wait
-	std::cin.get();
 
 	// Destroy the window. This will also destroy the surface
 	SDL_DestroyWindow( window );
 
 	// Quit SDL
 	SDL_Quit();
-
-
-
-
-
-
-
-
 
 	// End the program
 	return 0;
